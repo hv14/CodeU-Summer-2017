0735236440a465445515521364022cc4d407a486
Himani Vyas <himonal@Himanis-MacBook-Pro.local>
2017-06-15 09:32:47 -0500

    added info command for root panel

--- src/codeu/chat/client/commandline/Chat.java
+++ src/codeu/chat/client/commandline/Chat.java
@@ -23,6 +23,7 @@ import codeu.chat.client.core.Context;
 import codeu.chat.client.core.ConversationContext;
 import codeu.chat.client.core.MessageContext;
 import codeu.chat.client.core.UserContext;
+import codeu.chat.server.ServerInfo;
 
 public final class Chat {
 
@@ -107,6 +108,8 @@ public final class Chat {
         System.out.println("    Add a new user with the given name.");
         System.out.println("  u-sign-in <name>");
         System.out.println("    Sign in as the user with the given name.");
+        System.out.println("  info");
+        System.out.println("    Display all the info of the current system");
         System.out.println("  exit");
         System.out.println("    Exit the program.");
       }
@@ -178,6 +181,23 @@ public final class Chat {
           }
         }
         return null;
+      }
+    });
+
+    // info
+    //
+    // Display the start time of the server when the user types
+    // "info" while on the root panel.
+    //
+    panel.register("u-info", new Panel.Command() {
+      @Override
+      public void invoke(Scanner args) {
+        final ServerInfo info = context.getInfo();
+        if (info == null) {
+          System.out.println("No info was found");
+        } else {
+          System.out.println("start time: " + context.getInfo().startTime);
+        }
       }
     });
 
